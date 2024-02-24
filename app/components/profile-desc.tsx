cec960886a6c1bdf56e22093c0d0909e62ddd27c
Abdoulaye BA <evilbas1038@gmail.com>
2024-02-24 13:53:35 +0100

    update

--- app/components/profile-desc.tsx
+++ app/components/profile-desc.tsx
@@ -31,8 +31,9 @@ const ProfileDescPage = () => {
             ou &quot;semi-agile&quot;. Je suis très motivé et je suis prêt à
             relever de nouveaux défis. Mes compétences en DevOps me permettent
             de mettre en place un environnement opérationnel intégrant les TDD
-            et facilitant l'évolution, l'orchestration et le déploiement des
-            applications; ce qui est un atout pour une entreprise.
+            et facilitant l&apos;évolution, l&apos;orchestration et le
+            déploiement des applications; ce qui est un atout pour une
+            entreprise.
             <RiDoubleQuotesL
               className="relative -bottom-2 inline rotate-180 text-3xl"
               fill="yellow"
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -39,10 +39,10 @@ const ProjectsPage = () => {
         <h3 className="text-6xl">In progress</h3>
         <p className="md:mx-40">
           These projects are in progress and will be available soon on the web.
-          Actually, I'm working on the backend of these projects. If you want to
-          know more about these projects, please contact me. I will be happy to
-          share with you the progress of these projects. Please, click the
-          button below to send me an email.
+          Actually, I&apos;m working on the backend of these projects. If you
+          want to know more about these projects, please contact me. I will be
+          happy to share with you the progress of these projects. Please, click
+          the button below to send me an email.
         </p>
         <div className="flex justify-center mt-6">
           <span className="inline-flex rounded-md shadow-sm">
