ccb7e39a4b3eb75e976bf4b85faa2f185cf991b7
Abdoulaye BA <evilbas1038@gmail.com>
2024-02-24 13:51:15 +0100

    update

---
+++ app/footer/page.tsx
@@ -0,0 +1,16 @@
+const FooterPage = () => {
+  return (
+    <>
+      <footer className="footer p-10 bg-neutral text-neutral-content">
+        <aside>
+          <p>
+            Asin.
+            <br />© 2024
+          </p>
+        </aside>
+      </footer>
+    </>
+  );
+};
+
+export default FooterPage;
