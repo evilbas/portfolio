584e59d01dd06964095e6fe6d4ef9bc63969f81b
Abdoulaye BA <evilbas1038@gmail.com>
2024-02-24 01:18:57 +0100

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,18 @@
-const About = () => {
-  return <div>About</div>;
+import PDFViewer from "../components/pdf-viewer";
+
+const AboutPage = () => {
+  return (
+    <div className="text-center">
+      <h1 className="m-2 text-center text-4xs">Currículum Vitae</h1>
+      <PDFViewer pdfUrl="files/asincv.pdf" />
+
+      <button className="my-3 btn btn-neutral">
+        <a href="files/asincv.pdf" download="asincv.pdf">
+          Télécharcher mon CV
+        </a>
+      </button>
+    </div>
+  );
 };
 
-export default About;
+export default AboutPage;
